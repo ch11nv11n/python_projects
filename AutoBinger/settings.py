6ebfd30aa2ea879f156427f4d8a99bb0a9c13b2d
ch11nv11n <joedejesus91@gmail.com>
2023-01-23 14:09:34 -0600

    Updating settings

--- AutoBinger/settings.py
+++ AutoBinger/settings.py
@@ -3,5 +3,12 @@ import os
 
 dotenv.load_dotenv('.env')
 
+# setting username and password securely
 USERNAME = os.environ['USERNAME']
 PASSWORD = os.environ['PASSWORD']
+
+# setting current working dir
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+# setting search string
+BINGURL = 'http://www.bing.com/search?q='
