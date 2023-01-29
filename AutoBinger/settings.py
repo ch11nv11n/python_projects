e39391a8b2f0f75c1fbc357c7fe6fb3668a22c98
ch11nv11n <joedejesus91@gmail.com>
2023-01-28 22:43:01 -0600

    Updating settings file

--- AutoBinger/settings.py
+++ AutoBinger/settings.py
@@ -4,11 +4,19 @@ import os
 dotenv.load_dotenv('.env')
 
 # setting username and password securely
-USERNAME = os.environ['USERNAME']
+USERNAME = os.environ['MS_USERNAME']
 PASSWORD = os.environ['PASSWORD']
+
+# EMAIL SETUP
+SMTP_PORT = os.environ['SMTP_PORT']
+SMTP_SERVER = os.environ['SMTP_SERVER']
+EMAIL_PASSWORD = os.environ['EMAIL_PASSWORD']
+FROM_ADDR = os.environ['FROM_ADDR']
+TO_ADDR = os.environ['TO_ADDR']
 
 # setting current working dir
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+MSDRIVER = __location__ + '\\' +os.environ['EDGEDRIVER']
 
 # setting search string
 BINGURL = 'http://www.bing.com/search?q='
